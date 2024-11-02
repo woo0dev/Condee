4608dbbf1e338a073922d881ef2906e3a6e3ca37
woo0 <57060443+woo0dev@users.noreply.github.com>
2024-11-02 18:41:43 +0900

    Feat/MainScene-Modify MainSceneViewUITests

--- Condee/CondeeUITests/MainSceneViewUITests.swift
+++ Condee/CondeeUITests/MainSceneViewUITests.swift
@@ -14,16 +14,30 @@ final class MainSceneViewUITests: XCTestCase {
 		let app = XCUIApplication()
 		app.launch()
     }
+	
+	@MainActor
+	func testElementsMainSceneView() throws {
+		let app = XCUIApplication()
+		app.launch()
+		
+		let addButton = app.buttons["addButton"]
+		XCTAssertTrue(addButton.exists, "새로 만들기 버튼이 없습니다.")
+		
+		let appNameLabel = app.staticTexts["Condee"]
+		XCTAssertTrue(appNameLabel.exists, "앱 이름이 표시되지 않습니다.")
+		
+		for index in 0..<4 {
+			let imageElement = app.images["image\(index)"]
+			XCTAssertTrue(imageElement.exists, "이미지\(index)가 표시되지 않습니다.")
+		}
+	}
 
     @MainActor
     func testAddButtonOpensCreateCustomImageView() throws {
         let app = XCUIApplication()
         app.launch()
 
-		let addButton = app.buttons["addButton"]
-		XCTAssertTrue(addButton.exists, "새로 만들기 버튼이 없습니다.")
-		
-		addButton.tap()
+		app.buttons["addButton"].tap()
 		
 		let createCustomImageView = app.otherElements["CreateCustomImageView"]
 		XCTAssertTrue(createCustomImageView.exists, "CreateCustomImageView를 불러올 수 없습니다.")
