391d4ab8083248ae0bb6c200ee77339bb9a32774
woo0 <57060443+woo0dev@users.noreply.github.com>
2024-11-04 14:01:11 +0900

    Feat/MainScene-Modify MainSceneViewModel

--- Condee/Condee/Presentation/MainScene/ViewModel/MainSceneViewModel.swift
+++ Condee/Condee/Presentation/MainScene/ViewModel/MainSceneViewModel.swift
@@ -14,14 +14,10 @@ final class MainSceneViewModel: ObservableObject {
 	@Published var customImages: [CustomImage] = []
 	
 	private let fetchAllCustomImagesUseCase: FetchAllCustomImagesUseCase
-	private let createCustomImageUseCase: CreateCustomImageUseCase
-	private let deleteCustomImageUseCase: DeleteCustomImageUseCase
 	private var cancellables = Set<AnyCancellable>()
 	
-	init(fetchIAllCustomImagesUseCase: FetchAllCustomImagesUseCase, createCustomImageUseCase: CreateCustomImageUseCase, deleteCustomImageUseCase: DeleteCustomImageUseCase) {
+	init(fetchIAllCustomImagesUseCase: FetchAllCustomImagesUseCase) {
 		self.fetchAllCustomImagesUseCase = fetchIAllCustomImagesUseCase
-		self.createCustomImageUseCase = createCustomImageUseCase
-		self.deleteCustomImageUseCase = deleteCustomImageUseCase
 	}
 	
 	func fetchAll() {
@@ -35,36 +31,6 @@ final class MainSceneViewModel: ObservableObject {
 				}
 			}, receiveValue: { [weak self] images in
 				self?.customImages = images
-			})
-			.store(in: &cancellables)
-	}
-	
-	func addCustomImage(with url: URL) {
-		createCustomImageUseCase.execute(imageURL: url)
-			.sink(receiveCompletion: { completion in
-				switch completion {
-				case .finished:
-					break
-				case .failure(let error):
-					print("Failed to create Custom Image: \(error)")
-				}
-			}, receiveValue: { [weak self] _ in
-				self?.fetchAll()
-			})
-			.store(in: &cancellables)
-	}
-	
-	func deleteCustomImage(_ image: CustomImage) {
-		deleteCustomImageUseCase.execute(customImage: image)
-			.sink(receiveCompletion: { completion in
-				switch completion {
-				case .finished:
-					break
-				case .failure(let error):
-					print("Failed to create Custom Image: \(error)")
-				}
-			}, receiveValue: { [weak self] _ in
-				self?.fetchAll()
 			})
 			.store(in: &cancellables)
 	}
